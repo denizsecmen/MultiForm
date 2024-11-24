913f515cb92526fdd63ff6552c2812b4f6b54c5b
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-11-24 20:56:20 +0300

    Info divs added.

--- src/components/progress/progress.tsx
+++ src/components/progress/progress.tsx
@@ -4,15 +4,19 @@ export default function Progress() {
     <div className={styles.progressSign}>
       <div className={styles.circle}>
         <div className={styles.pnumber}>1</div>
+        <div className={styles.info}></div>
       </div>
       <div className={styles.circle}>
         <div className={styles.pnumber}>2</div>
+        <div className={styles.info}></div>
       </div>
       <div className={styles.circle}>
         <div className={styles.pnumber}>3</div>
+        <div className={styles.info}></div>
       </div>
       <div className={styles.circle}>
         <div className={styles.pnumber}>4</div>
+        <div className={styles.info}></div>
       </div>
     </div>
   );

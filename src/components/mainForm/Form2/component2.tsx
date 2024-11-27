8184edbf5195b24c596d2d206f443d054d63f9c3
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-11-27 16:51:51 +0300

    Component 2 base HTML finished

--- src/components/mainForm/Form2/component2.tsx
+++ src/components/mainForm/Form2/component2.tsx
@@ -3,6 +3,22 @@ export default function component2() {
   return (
     <div className={styles.component}>
       <div className={styles.title}> Online service</div>
+      <div className={styles.subtitle}>
+        Add-ons help enhance your gaming experience.
+      </div>
+      <div className={styles.selections}>
+        <div className={styles.selection}>
+          Online service Access to multiplayer games +$1/mo
+        </div>
+        <div className={styles.selection2}>
+          Larger storage Extra 1TB of cloud save +$2/mo
+        </div>
+        <div className={styles.selection3}>
+          Customizable Profile Custom theme on your profile +$2/mo
+        </div>
+        <div className={styles.switch}></div>
+        <div className={styles.buttons}></div>
+      </div>
     </div>
   );
 }

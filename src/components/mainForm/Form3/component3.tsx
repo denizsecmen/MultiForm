7aa5b9913f1c0cfa19b75fcba2adac06308622c2
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-12-02 01:34:53 +0300

    Check system was made

--- src/components/mainForm/Form3/component3.tsx
+++ src/components/mainForm/Form3/component3.tsx
@@ -1,20 +1,22 @@
 import context from '@/context/context';
 import styles from './style/style.module.css';
-import { useContext } from 'react';
+import { useContext, useReducer } from 'react';
 export default function component3() {
+  function changer(
+    stat: [boolean, boolean, boolean],
+    action: { type: String }
+  ) {
+    if (action.type == '1') {
+      stat[0] = !stat[0];
+    } else if (action.type == '2') {
+      stat[1] = !stat[1];
+    } else {
+      stat[0] = !stat[2];
+    }
+    return stat;
+  }
+  let [state, dispatch] = useReducer(changer, [false, false, false]);
   let [progress, chprogress] = useContext(context);
-  /*  Online service
-  Access to multiplayer games
-  +$1/mo
-
-  Larger storage
-  Extra 1TB of cloud save
-  +$2/mo
-
-  Customizable Profile
-  Custom theme on your profile
-  +$2/mo
-*/
   return (
     <div className={styles.component}>
       <div className={styles.headers}>
@@ -25,28 +27,52 @@ export default function component3() {
       </div>
       <div className={styles.checks}>
         <div className={styles.check}>
-          <input type="checkbox" />
-          <div className={styles.info}>
-            <div className={styles.ctitle}>Online service</div>
-            <div className={styles.csubtitle}>Access to multiplayer games</div>
+          <div className={styles.f}>
+            <input
+              type="checkbox"
+              checked={state[0]}
+              className={styles.checkbox}
+            />
+            <div className={styles.info}>
+              <div className={styles.ctitle}>Online service</div>
+              <div className={styles.csubtitle}>
+                Access to multiplayer games
+              </div>
+            </div>
           </div>
           <div className={styles.price}>+$1/mo</div>
         </div>
         <div className={styles.check}>
-          <input type="checkbox" />
-          <div className={styles.info}>
-            <div className={styles.ctitle}> Larger storage</div>
-            <div className={styles.csubtitle}> Extra 1TB of cloud save</div>
+          <div
+            className={styles.f}
+            onClick={() => {
+              dispatch({ type: '1' });
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={state[1]}
+              className={styles.checkbox}
+            />
+            <div className={styles.info}>
+              <div className={styles.ctitle}> Larger storage</div>
+              <div className={styles.csubtitle}> Extra 1TB of cloud save</div>
+            </div>
           </div>
           <div className={styles.price}> +$2/mo</div>
         </div>
         <div className={styles.check}>
-          <input type="checkbox" />
-          <div className={styles.info}>
-            <div className={styles.ctitle}> Customizable Profile</div>
-            <div className={styles.csubtitle}>
-              {' '}
-              Custom theme on your profile
+          <div className={styles.f}>
+            <input
+              type="checkbox"
+              checked={state[2]}
+              className={styles.checkbox}
+            />
+            <div className={styles.info}>
+              <div className={styles.ctitle}> Customizable Profile</div>
+              <div className={styles.csubtitle}>
+                Custom theme on your profile
+              </div>
             </div>
           </div>
           <div className={styles.price}> +$2/mo</div>

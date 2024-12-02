7eb3ec516200d932d90e5e7f1af08cdb86b492cf
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-12-02 14:34:27 +0300

    Component3 was finished

--- src/components/mainForm/Form3/component3.tsx
+++ src/components/mainForm/Form3/component3.tsx
@@ -1,32 +1,36 @@
 import context from '@/context/context';
 import styles from './style/style.module.css';
 import { useContext, useReducer } from 'react';
-export default function component3() {
-  function changer(
-    stat: [boolean, boolean, boolean],
-    action: { type: String }
-  ) {
-    if (action.type == '1') {
-      stat[0] = !stat[0];
-    } else if (action.type == '2') {
-      stat[1] = !stat[1];
-    } else {
-      stat[0] = !stat[2];
+
+type State = [boolean, boolean, boolean];
+type Action = { type: number };
+
+export default function Component3() {
+  function changer(stat: State, action: Action): State {
+    switch (action.type) {
+      case 1:
+        return [!stat[0], stat[1], stat[2]];
+      case 2:
+        return [stat[0], !stat[1], stat[2]];
+      case 3:
+        return [stat[0], stat[1], !stat[2]];
+      default:
+        return stat;
     }
-    return stat;
   }
   let [state, dispatch] = useReducer(changer, [false, false, false]);
   let [progress, chprogress] = useContext(context);
+
   return (
     <div className={styles.component}>
       <div className={styles.headers}>
-        <div className={styles.title}> Pick add-ons</div>
+        <div className={styles.title}>Pick add-ons</div>
         <div className={styles.subtitle}>
           Add-ons help enhance your gaming experience.
         </div>
       </div>
       <div className={styles.checks}>
-        <div className={styles.check}>
+        <div className={styles.check} onClick={() => dispatch({ type: 1 })}>
           <div className={styles.f}>
             <input
               type="checkbox"
@@ -42,26 +46,21 @@ export default function component3() {
           </div>
           <div className={styles.price}>+$1/mo</div>
         </div>
-        <div className={styles.check}>
-          <div
-            className={styles.f}
-            onClick={() => {
-              dispatch({ type: '1' });
-            }}
-          >
+        <div className={styles.check} onClick={() => dispatch({ type: 2 })}>
+          <div className={styles.f}>
             <input
               type="checkbox"
               checked={state[1]}
               className={styles.checkbox}
             />
             <div className={styles.info}>
-              <div className={styles.ctitle}> Larger storage</div>
-              <div className={styles.csubtitle}> Extra 1TB of cloud save</div>
+              <div className={styles.ctitle}>Larger storage</div>
+              <div className={styles.csubtitle}>Extra 1TB of cloud save</div>
             </div>
           </div>
-          <div className={styles.price}> +$2/mo</div>
+          <div className={styles.price}>+$2/mo</div>
         </div>
-        <div className={styles.check}>
+        <div className={styles.check} onClick={() => dispatch({ type: 3 })}>
           <div className={styles.f}>
             <input
               type="checkbox"
@@ -69,30 +68,20 @@ export default function component3() {
               className={styles.checkbox}
             />
             <div className={styles.info}>
-              <div className={styles.ctitle}> Customizable Profile</div>
+              <div className={styles.ctitle}>Customizable Profile</div>
               <div className={styles.csubtitle}>
                 Custom theme on your profile
               </div>
             </div>
           </div>
-          <div className={styles.price}> +$2/mo</div>
+          <div className={styles.price}>+$2/mo</div>
         </div>
       </div>
       <div className={styles.buttons}>
-        <button
-          className={styles.back}
-          onClick={() => {
-            chprogress('2');
-          }}
-        >
+        <button className={styles.back} onClick={() => chprogress('2')}>
           Go Back
         </button>
-        <button
-          className={styles.next}
-          onClick={() => {
-            chprogress('4');
-          }}
-        >
+        <button className={styles.next} onClick={() => chprogress('4')}>
           Next Step
         </button>
       </div>
